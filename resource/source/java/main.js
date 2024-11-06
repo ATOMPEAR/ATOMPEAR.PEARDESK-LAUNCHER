bafabf4ad3142f091c73ec3d89faa532c74e027e
ATOMPEAR <ATOMPEAR@OUTLOOK.COM>
2024-11-05 18:50:51 -0700

    MAIN COMMIT
    -- ADD TRAY ICON WITH FAVICON.ICO
    -- MADE WINDOW MINIMIZE TO TRAY WHEN MINIMIZE BUTTON IS CLICKED
    -- MADE NATIVE NOTIFICATION WHEN WINDOW IN MINIMIZED

--- resource/source/java/main.js
+++ resource/source/java/main.js
@@ -1,9 +1,10 @@
 'use strict';
 
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, Tray, Menu, Notification } = require('electron');
 const path = require('path');
 
 let mainWindow;
+let tray;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -23,16 +24,64 @@ function createWindow() {
   mainWindow.loadFile(indexPath);
 }
 
+function createTray() {
+  tray = new Tray(path.join(__dirname, '../../assets/images/favicons/favicon.ico'));
+  
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Show App',
+      click: () => {
+        mainWindow.show();
+      }
+    },
+    {
+      label: 'Quit',
+      click: () => {
+        app.quit();
+      }
+    }
+  ]);
+
+  tray.setToolTip('Electron App');
+  tray.setContextMenu(contextMenu);
+
+  // Double click on tray icon shows the window
+  tray.on('double-click', () => {
+    mainWindow.show();
+  });
+}
+
+function showNotification() {
+  const notification = new Notification({
+    title: 'App Minimized',
+    body: 'App is running in the system tray',
+    icon: path.join(__dirname, '../../assets/images/favicons/favicon.ico')
+  });
+  notification.show();
+}
+
 app.whenReady().then(() => {
   createWindow();
+  createTray();
 
   // Handle window controls
   ipcMain.on('minimize-window', () => {
-    mainWindow.minimize();
+    mainWindow.hide(); // Hide instead of minimize
+    showNotification();
   });
 
   ipcMain.on('close-window', () => {
-    mainWindow.close();
+    app.quit(); // Quit the entire application
+  });
+
+  // Prevent window from being destroyed when closed
+  mainWindow.on('close', (event) => {
+    if (!app.isQuitting) {
+      event.preventDefault();
+      mainWindow.hide();
+      return false;
+    }
+    return true;
   });
 
   app.on('activate', () => {
@@ -42,8 +91,14 @@ app.whenReady().then(() => {
   });
 });
 
+// Quit the app when all windows are closed (except on macOS)
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
+});
+
+// Clean up before quitting
+app.on('before-quit', () => {
+  app.isQuitting = true;
 });
--- resource/source/java/preload.js
+++ resource/source/java/preload.js
@@ -5,4 +5,5 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
   minimizeWindow: () => ipcRenderer.send('minimize-window'),
   closeWindow: () => ipcRenderer.send('close-window'),
+  // Add any additional APIs here
 });
